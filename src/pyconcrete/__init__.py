f7b1dfea0bc12fd9b10a7028e71fd5ad1072e7c0
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2018-05-07 17:06:38 -0400

    Allow the key to be specified at runtime
    Also use cryptography for the encryption instead of a custom
    build C module.

--- src/pyconcrete/__init__.py
+++ src/pyconcrete/__init__.py
@@ -6,6 +6,7 @@ from os.path import join, exists, isdir
 import os
 import base64
 
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
